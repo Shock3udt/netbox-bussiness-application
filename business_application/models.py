626370f1e97b1ba8d9fa7e5eabe71481073b293a
network <imitruk@redhat.com>
2024-12-15 19:30:11 +0100

    fix(breaking): revert appcode being primary key business application

--- business_application/models.py
+++ business_application/models.py
@@ -8,7 +8,7 @@ class BusinessApplication(NetBoxModel):
     """
     A model representing a business application in the organization.
     """
-    appcode = models.CharField(max_length=20, unique=True, primary_key=True)
+    appcode = models.CharField(max_length=20, unique=True)
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True)
     owner = models.CharField(max_length=100)
