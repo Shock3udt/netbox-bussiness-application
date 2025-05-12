ae4a478dd9ed531f833b217cafe1c7a197b5838a
tkloda <72545392+tkloda@users.noreply.github.com>
2025-05-12 16:34:44 +0200

    Update views.py

--- business_application/api/views.py
+++ business_application/api/views.py
@@ -1,7 +1,9 @@
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.viewsets import ModelViewSet, ReadOnlyModelViewSet
 from business_application.models import BusinessApplication
-from business_application.api.serializers import BusinessApplicationSerializer
+from business_application.api.serializers import BusinessApplicationSerializer, DeviceWithApplicationsSerializer
 from rest_framework.permissions import IsAuthenticated
+from dcim.models import Device
+
 
 class BusinessApplicationViewSet(ModelViewSet):
     """
@@ -23,3 +25,11 @@ class BusinessApplicationViewSet(ModelViewSet):
         if appcode:
             queryset = queryset.filter(appcode__iexact=appcode)
         return queryset
+
+class DeviceWithApplicationsViewSet(ReadOnlyModelViewSet):
+    """
+    API endpoint that lists Devices with their related Business Applications.
+    """
+    queryset = Device.objects.prefetch_related('business_applications').all()
+    serializer_class = DeviceWithApplicationsSerializer
+    permission_classes = [IsAuthenticated]
