af8c91858e7c409c68482f06ecd8c18b6a4281f4
tkloda <72545392+tkloda@users.noreply.github.com>
2025-05-12 16:36:22 +0200

    Update urls.py

--- business_application/api/urls.py
+++ business_application/api/urls.py
@@ -1,7 +1,8 @@
 from rest_framework.routers import DefaultRouter
-from business_application.api.views import BusinessApplicationViewSet
+from business_application.api.views import BusinessApplicationViewSet, DeviceWithApplicationsViewSet
 
 router = DefaultRouter()
 router.register(r'business-applications', BusinessApplicationViewSet, basename='businessapplication')
+router.register(r'devices-with-apps', DeviceWithApplicationsViewSet, basename='devicewithapps')
 
 urlpatterns = router.urls
