2cb4a3cb79553f81b3eb15180d568437c91cb867
tkloda <72545392+tkloda@users.noreply.github.com>
2025-05-13 07:29:05 +0200

    Update urls.py

--- business_application/api/urls.py
+++ business_application/api/urls.py
@@ -1,8 +1,17 @@
 from rest_framework.routers import DefaultRouter
-from business_application.api.views import BusinessApplicationViewSet, DeviceWithApplicationsViewSet
+from business_application.api.views import BusinessApplicationViewSet, DeviceDownstreamAppsViewSet
 
 router = DefaultRouter()
 router.register(r'business-applications', BusinessApplicationViewSet, basename='businessapplication')
-router.register(r'devices-with-apps', DeviceWithApplicationsViewSet, basename='devicewithapps')
 
-urlpatterns = router.urls
+device_downstream_view = DeviceDownstreamAppsViewSet.as_view({
+    'get': 'downstream_applications'
+})
+
+urlpatterns = router.urls + [
+    path(
+        'devices/<int:pk>/downstream-applications/',
+        device_downstream_view,
+        name='device-downstream-applications'
+    )
+]
