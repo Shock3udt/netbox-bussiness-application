8f04de182cec89fc01217dbb887f3c1f04503933
tkloda <72545392+tkloda@users.noreply.github.com>
2025-05-13 09:42:04 +0200

    Update urls.py

--- business_application/api/urls.py
+++ business_application/api/urls.py
@@ -4,15 +4,4 @@ from business_application.api.views import BusinessApplicationViewSet, DeviceDow
 
 router = DefaultRouter()
 router.register(r'business-applications', BusinessApplicationViewSet, basename='businessapplication')
-
-device_downstream_view = DeviceDownstreamAppsViewSet.as_view({
-    'get': 'downstream_applications'
-})
-
-urlpatterns = router.urls + [
-    path(
-        'devices/<int:pk>/downstream-applications/',
-        device_downstream_view,
-        name='device-downstream-applications'
-    )
-]
+router.register(r'devices', DeviceDownstreamAppsViewSet, basename='devicedownstreamapps')
