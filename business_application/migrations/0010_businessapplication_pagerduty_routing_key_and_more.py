b6bd600f167c2677b7d55e63f02da75e4d04c617
network <imitruk@redhat.com>
2025-11-28 22:42:38 +0100

    resolve multiple leaf migrations

--- business_application/migrations/0010_businessapplication_pagerduty_routing_key_and_more.py
+++ business_application/migrations/0010_businessapplication_pagerduty_routing_key_and_more.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('business_application', '0008_add_external_workflow'),
+        ('business_application', '0009_externalworkflow_tags_workflowexecution'),
     ]
 
     operations = [
